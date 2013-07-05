6be2866a341f5c96755a7ded200ed06d06500f28
kurosaki <kuro@dev.kurochan.org>
2013-07-06 04:11:37 +0900

    post

--- kyuko_bot.rb
+++ kyuko_bot.rb
@@ -2,7 +2,22 @@
 require 'rubygems'
 require 'sinatra'
 require 'thin'
+require 'json'
+
+load 'config.rb'
 
 get '/' do
     "Hello World!"
+end
+
+post "#{MAIL}" do
+    message = JSON.parse(params[:message]);
+    return nil unless message
+    twitter = Twitter::Client.new(
+        :consumer_key => CONSUMER_KEY,
+        :consumer_secret => CONSUMER_SECRET,
+        :oauth_token => ACCESS_TOKEN,
+        :oauth_token_secret => ACCESS_TOKEN_SECRET
+    )   
+    twitter.update "received"
 end
